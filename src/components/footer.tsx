dfa704be462852722883b8473f27bae5d4bb2e69
Esteban <wiwiwixd732@gmail.com>
2023-11-15 13:58:58 -0800

    feat: Footer.tsx removed and set in layout folder

--- src/components/footer.tsx
+++
@@ -1,7 +0,0 @@
-const Footer = () => {
-  return (
-    <p>some here</p>
-  )
-}
-
-export default Footer
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export { default as Language } from './language'
 export { default as Search } from './search'
 export { default as Info } from './info'
-export { default as Footer } from './footer'
 export { default as Card } from './card'
 
 export {
