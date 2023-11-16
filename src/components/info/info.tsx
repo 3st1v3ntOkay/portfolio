f4ca574eda44b8afaecc0b42af949988b88cfd1c
Esteban <wiwiwixd732@gmail.com>
2023-11-16 09:52:47 -0800

    feat: Info component ready

---
+++ src/components/info/info.tsx
@@ -0,0 +1,11 @@
+import style from './info.module.css'
+
+const Info = () => {
+  return (
+    <p className={style.info}>
+      This page is still under construction, sorry if you see any visual or functional bug, just press Ctrl B and go report section.
+    </p>
+  )
+}
+
+export default Info
