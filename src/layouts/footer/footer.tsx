318db268f31ce42668e0b190517260d948544651
Esteban <wiwiwixd732@gmail.com>
2023-11-16 15:18:55 -0800

    feat: Footer template finished

--- src/layouts/footer/footer.tsx
+++ src/layouts/footer/footer.tsx
@@ -1,6 +1,27 @@
+import Image from 'next/image';
+import e from './images/e.svg';
+import Link from 'next/link';
+import style from './footer.module.css';
+
 const Footer = () => {
   return (
-    <p>some here</p>
+    <footer className={style.footer}>
+      <div className={style.space}>
+        <p className={style.author}>@estebanjimenezvaldez</p>
+        <div className={style["content-x"]}>
+          <Image
+            src={e}
+            width={32}
+            height={32}
+            alt="e"
+          />
+          <div className={style["content-y"]}>
+            <p className={style.version}>version 1.0.0</p>
+            <Link href='#' className={style.url}>me.com</Link>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
