6521cc7ea8a681424920a5622dd02acc98ba102c
Esteban <wiwiwixd732@gmail.com>
2023-11-17 10:26:23 -0800

    feat: nothing relevant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
 import { Footer, Top } from '@/layouts'
-import styles from './page.module.css'
+import style from './page.module.css'
+import { Card, CardPerson, CardPersonMini } from '@/components'
+import { catFace1 } from '@/assets/emoji/emoji-cat';
 
 export default function Home() {
+  const info = [{
+    title: "how I work",
+    description: "Here you can find the programming languages and technologies I use for website development.",
+  }];
+
   return (
     <>
       <Top />
 
-      <main className={styles.main}>
+      <main className={style.main}>
         <h1>Welcome.</h1>
+
+        <Card
+          title={info[0].title}
+          description={info[0].description}
+          image={catFace1}
+        />
+
+        <CardPerson image={catFace1}>
+          Jack Herrington
+        </CardPerson>
+
+        <CardPersonMini image={catFace1}>
+          Jack Herrington
+        </CardPersonMini>
       </main>
 
       <Footer />
