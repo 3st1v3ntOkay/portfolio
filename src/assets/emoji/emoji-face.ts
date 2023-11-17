ea89e55e2f6538a333bd62a523e34319d9ba1e92
Esteban <wiwiwixd732@gmail.com>
2023-11-17 08:46:06 -0800

    feat: icons and images added

---
+++ src/assets/emoji/emoji-face.ts
@@ -0,0 +1,15 @@
+import faceAffection from './face/face-affection.svg'
+import faceGlasses from './face/face-glasses.svg'
+import faceSmilling2 from './face/face-smiling-2.svg'
+import faceTongue from './face/face-tongue.svg'
+import faceUnwell from './face/face-unwell.svg'
+import faceUnwell5 from './face/face-unwell-5.svg'
+
+export {
+  faceAffection,
+  faceGlasses,
+  faceSmilling2,
+  faceTongue,
+  faceUnwell,
+  faceUnwell5,
+}
