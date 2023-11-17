88a5a6bca2a5b6f11245054234b198541b1d5996
Esteban <wiwiwixd732@gmail.com>
2023-11-17 14:35:24 -0800

    feat: button switch to key component + try to fix layout in small devices, it has a mini solution and it's okay for now

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,10 +1,13 @@
+import { Key } from '..'
 import style from './search.module.css'
 
 const Search = () => {
   return (
     <div className={style['search-content']}>
       <p className={style.page}>Home</p>
-      <button className={style.button}>Ctrl B</button>
+      <Key>
+        Ctrl B
+      </Key>
     </div>
   )
 }
