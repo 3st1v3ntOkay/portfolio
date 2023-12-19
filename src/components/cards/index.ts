8d8c929d30721bf19f04d2758e10d7c0d0f217ab
Esteban <wiwiwixd732@gmail.com>
2023-12-19 10:21:57 -0800

    feat: barrel

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,5 +1,5 @@
-export {
-  Card,
-  CardPerson,
-  CardPersonMini,
-} from './card'
+export { Card, CardInfo } from './card';
+export { Information } from './information';
+export { IconTitle } from './icon-title';
+export { LinkManager } from './link-manager';
+export { Banner } from './banner';
