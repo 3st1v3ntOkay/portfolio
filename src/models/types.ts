caa84c52d8b55849fd247c5e5d0c642dd518d0f0
Esteban <wiwiwixd732@gmail.com>
2023-12-19 11:48:05 -0800

    feat: new interfaces added

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,3 +1,5 @@
+import type { StaticImageData } from "next/image"
+
 export interface ChildrenProps {
   children: React.ReactNode
 }
@@ -10,4 +12,30 @@ export interface CardProps {
   title: string
   description: string
   link: string
+}
+
+export interface Tutors {
+  id: string
+  avatar: StaticImageData
+  name: string
+  description: string
+  links: {
+    youtube: string,
+    github: string,
+  }
+}
+
+export interface Links {
+  id: string
+  image: StaticImageData
+  repository: string
+  url: string
+  year: number
+}
+
+export interface Pages {
+  id: string
+  page: string
+  description: string
+  url: string
 }
