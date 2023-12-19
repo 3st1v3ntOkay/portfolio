d3ea31c58a38465f0ee8ec267d720695c10ddd0d
Esteban <wiwiwixd732@gmail.com>
2023-12-19 14:13:11 -0800

    feat: main page more in form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,90 +1,62 @@
+/* Own */
 import { Footer, Top } from '@/layouts'
 import style from './page.module.css'
-import { ButtonHeroiconOutline, Card } from '@/components'
-import { catFace1 } from '@/assets/emoji/emoji-cat';
+import { Card, FillButton, IconTitle, Information } from '@/components'
+
+/* Nextjs */
+import Image from 'next/image';
 import Link from 'next/link';
 
-interface CardProps {
-  title: string
-  description: string
-  link: string
-}
+/* Imagenes */
+import { catFace1 } from '@/assets/emoji/emoji-cat';
+import { Arrow } from './svg';
+
+/* Data */
+import { tutors, github, pages } from './data/data';
 
 export default function Home() {
-  const info = [
-    {
-      title: "bugs page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/bugs',
-    },
-    {
-      title: "development page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/development',
-    },
-    {
-      title: "follow page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/follow',
-    },
-    {
-      title: "habbies page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/hobbies',
-    },
-    {
-      title: "personal page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/personal',
-    },
-    {
-      title: "work page",
-      description: "Here you can find the programming languages and technologies I use for website development.",
-      link: '/work',
-    },
-  ];
-
   return (
     <div className={style["content-grid"]}>
-      <div className={`${style["top"]} ${style.breakout}`}>
+      <div className={`${style.top} ${style.breakout}`}>
         <Top />
       </div>
 
-      <main className={`${style["main"]} ${style.breakout}`}>
-        <div className={style["content-main"]}>
-          <h1 className={style["title-main"]}>Welcome.</h1>
-          <p className={style["p-main"]}>I introduce myself as Esteban Jimenez Valdez (a 21 years old guy). A couple of years ago, I embarked on my programming journey. With the dedication and effort, I enthusiastically continue on this amazing path, eager to take on new challenges.</p>
+      <main className='breakout'>
+        <Information gap='wide'>
+          {pages.map(({
+            id,
+            page,
+            description,
+            url,
+            disabled,
+          }) => (
+            <Card outline key={id}>
+              <Information gap='normal'>
+                <IconTitle>
+                  <h4>{page}</h4>
+                  <Image
+                    src={catFace1}
+                    width={32}
+                    height={32}
+                    alt="cat smilling"
+                  />
+                </IconTitle>
 
-          {/* TODO */}
-          <div className={style["content-x-main"]}>
-            <Link href="https://github.com/3st1v3ntOkay" target='_blank'>
-              <ButtonHeroiconOutline left={false} right={true}>
-                Github
-              </ButtonHeroiconOutline>
-            </Link>
-            <Link href="https://www.linkedin.com/in/esteban-j-valdez/" target='_blank'>
-              <ButtonHeroiconOutline left={false} right={true}>
-                Linkedin
-              </ButtonHeroiconOutline>
-            </Link>
-          </div>
-        </div>
+                <p>{description}</p>
+              </Information>
 
-        {/* TODO */}
-        <div className={style["list-card"]}>
-          {info.map(({ title, description, link }: CardProps, index) => (
-            <Card
-              key={index}
-              title={title}
-              description={description}
-              image={catFace1}
-              link={link}
-            />
+              <Link href={url}>
+                <FillButton disabled={disabled}>
+                  check
+                  <Arrow color='light' disabled={disabled} />
+                </FillButton>
+              </Link>
+            </Card>
           ))}
-        </div>
+        </Information>
       </main>
 
-      <div className={`${style["bottom"]} ${style.breakout}`}>
+      <div className={`${style.bottom} ${style.breakout}`}>
         <Footer />
       </div>
     </div>
