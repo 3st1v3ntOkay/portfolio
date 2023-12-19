1c9bbce575a3a1e51b70a1c76b6a07fe93e437b3
Esteban <wiwiwixd732@gmail.com>
2023-12-19 14:51:02 -0800

    feat: 3 new data

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,5 +1,5 @@
 /* Own */
-import { Links, Tutors, Pages } from "@/models/types";
+import { Links, Tutors, Pages, Videogames, Music } from "@/models/types";
 import {
   alan,
   jack,
@@ -187,5 +187,164 @@ export const github: Links[] = [
     repository: 'javascript-next-level',
     url: 'https://github.com/3st1v3ntOkay/javascript-next-level',
     year: 2023,
+  },
+];
+
+export const videogames: Videogames[] = [
+  {
+    id: uuidv4(),
+    game: "Counter Strike",
+  },
+  {
+    id: uuidv4(),
+    game: "Apex Legends",
+  },
+  {
+    id: uuidv4(),
+    game: "Insurgency",
+  },
+  {
+    id: uuidv4(),
+    game: "Terraria",
+  },
+  {
+    id: uuidv4(),
+    game: "Minecraft",
+  },
+  {
+    id: uuidv4(),
+    game: "FIFA",
+  },
+  {
+    id: uuidv4(),
+    game: "Call of Duty",
+  },
+];
+
+export const sports: Videogames[] = [
+  {
+    id: uuidv4(),
+    game: "Football",
+  },
+  {
+    id: uuidv4(),
+    game: "Calisthenics",
+  },
+  {
+    id: uuidv4(),
+    game: "Running",
+  },
+];
+
+export const music: Music[] = [
+  {
+    id: uuidv4(),
+    name: "Ryo Fukui",
+    albums: ["Scenery", "My Favorite Tune", "Mello Dream", "A Letter From Slowboat"],
+  },
+  {
+    id: uuidv4(),
+    name: "Tatsuro Yamashita",
+    albums: ["Spacy", "Go Ahead!", "Moonglow", "Come Along", "Ride on Time", "For You", "Big Wave", "Pacific (with Haruomi Hosono and Shigeru Suzuki), Come Along II"],
+  },
+  {
+    id: uuidv4(),
+    name: "Coldplay",
+    albums: ["Parachutes", "X&Y", "Viva la Vida or Death and All His Friends", "Ghost Stories", "Everyday Life"],
+  },
+  {
+    id: uuidv4(),
+    name: "Anri",
+    albums: ["Timely!!"],
+  },
+  {
+    id: uuidv4(),
+    name: "Miki Matsubara",
+    albums: ["Pocket Park"],
+  },
+  {
+    id: uuidv4(),
+    name: "Ennio Marricone",
+    albums: ["Ennio Morricone - Sergio Leone Greatest Western Music of All Time (Remastered HQ Audio)"],
+  },
+  {
+    id: uuidv4(),
+    name: "Hajime Wakai, Mahito Yokota, Shiho Fujii, Takeshi Hama",
+    albums: ["The Legend of Zelda: Skyward Sword"],
+  },
+  {
+    id: uuidv4(),
+    name: "Toru Minegishi, Asuka Ota, Koji Kondo",
+    albums: ["The Legend of Zelda: Twilight Princess"],
+  },
+  {
+    id: uuidv4(),
+    name: "Koji Kondo, Toru Minegishi",
+    albums: ["The Legend of Zelda: Majora's Mask"],
+  },
+  {
+    id: uuidv4(),
+    name: "Johann Sebastian Bach",
+    albums: ["Violin Concertos", "French Suites", "Suites for Guitar", "English Suites", "Goldeng Variations", "Partitas"],
+  },
+  {
+    id: uuidv4(),
+    name: "Frédéric François Chopin​",
+    albums: ["Nocturnos"],
+  },
+  {
+    id: uuidv4(),
+    name: "Franz Liszt​",
+    albums: ["Hugarian Rhapsodies", "La campanella"],
+  },
+  {
+    id: uuidv4(),
+    name: "Gustav Mahler​",
+    albums: ["Symphonies"],
+  },
+  {
+    id: uuidv4(),
+    name: "Johannes Brahms​",
+    albums: ["Hungarian Dances"],
+  },
+  {
+    id: uuidv4(),
+    name: "Johann Baptist Strauss II​",
+    albums: ["Waltz", "Polkas", "Operetas"],
+  },
+  {
+    id: uuidv4(),
+    name: "Georg Friedrich Händel​",
+    albums: ["Waltz", "Concertos"],
+  },
+  {
+    id: uuidv4(),
+    name: "Giuseppe Fortunino Francesco Verdi​",
+    albums: ["Operas"],
+  },
+  {
+    id: uuidv4(),
+    name: "Niccolò Paganini",
+    albums: ["24 Caprichos", "Devil's trill Sonata", "Ghiribizzi"],
+  },
+  {
+    id: uuidv4(),
+    name: "Makis Ablianitis",
+    albums: ["Bahar"],
+  },
+  {
+    id: uuidv4(),
+    name: "Brazilian regional music",
+    albums: ["Sol de Bossa: Bossa Nova", "Sol de Bossa: Bossa Felicidade", "Sol de Bossa: Bossa Beleza"],
+  },
+  {
+    id: uuidv4(),
+    name: "Masayoshi Takanaka",
+    albums: ["All Of Me"],
+  },
+  {
+    id: uuidv4(),
+    name: "Casiopea",
+    albums: ["Galactic Funk", "Casiopea vs Tsquare - Fightman"],
   },
 ];
