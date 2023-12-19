9500fad4079c4c8420bb1527bba29266cdb7975b
Esteban <wiwiwixd732@gmail.com>
2023-12-19 10:29:22 -0800

    feat: global interfaces/types here

---
+++ src/models/types.ts
@@ -0,0 +1,13 @@
+export interface ChildrenProps {
+  children: React.ReactNode
+}
+
+export interface SVGProps {
+  color: string
+}
+
+export interface CardProps {
+  title: string
+  description: string
+  link: string
+}
