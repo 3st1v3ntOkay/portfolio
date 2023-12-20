dfa862df3fd764cff43ffc1e43a10a6584ef0c2d
Esteban <wiwiwixd732@gmail.com>
2023-12-20 10:23:31 -0800

    feat: all ready to be use

---
+++ src/app/work/page.tsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import style from './page.module.css'
+import { Footer, Top } from '@/layouts'
+import Link from 'next/link'
+import Image from 'next/image'
+import { HeaderComp, Information, Row, Key } from '@/components'
+import { future, old, stack } from '../data/data'
+
+interface CurrentProps {
+  id: number | string
+  name: string
+  image: string
+  alt: string
+}
+
+const Work = () => {
+  return (
+    <div className={style["content-grid"]}>
+      <div className={`${style["top"]} ${style.breakout}`}>
+        <Top />
+      </div>
+
+      <main className={`${style["main"]} ${style.breakout}`}>
+        <HeaderComp>
+          <Link href="/">Home</Link>
+          <h1 className={style["title-main"]}>Work.</h1>
+          <p className={style["p-main"]}>Here is a brief overview of my technology expertise:</p>
+        </HeaderComp>
+
+        <Information gap='wide'>
+          <p>I use this:</p>
+
+          <Row>
+            {stack.map(({ id, name, image, alt }) => (
+              <Key key={id}>
+                <Image
+                  src={image}
+                  width={16}
+                  height={16}
+                  alt={alt}
+                />
+                {name}
+              </Key>
+            ))}
+          </Row>
+        </Information>
+
+        <Information gap='wide'>
+          <p>I used to use this:</p>
+
+          <Row>
+            {old.map(({ id, name, image, alt }) => (
+              <Key key={id}>
+                <Image
+                  src={image}
+                  width={16}
+                  height={16}
+                  alt={alt}
+                />
+                {name}
+              </Key>
+            ))}
+          </Row>
+        </Information>
+
+        <Information gap='wide'>
+          <p>I want to use this:</p>
+
+          <Row>
+            {future.map(({ id, name, image, alt }) => (
+              <Key key={id}>
+                <Image
+                  src={image}
+                  width={16}
+                  height={16}
+                  alt={alt}
+                />
+                {name}
+              </Key>
+            ))}
+          </Row>
+        </Information>
+      </main>
+
+      <div className={`${style["bottom"]} ${style.breakout}`}>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Work
