d300ed1932c0bf2bc10fbad6ae24ab49fcc88ebe
Esteban <wiwiwixd732@gmail.com>
2023-12-20 14:08:04 -0800

    feat + fixes: blog page more in form

---
+++ src/app/blog/[article]/page.tsx
@@ -0,0 +1,69 @@
+import { DateComp, HeaderComp, Row } from "@/components";
+import Link from "next/link";
+import style from './page.modules.css';
+import { articlePage, articles } from '@/app/data/data';
+import Image from "next/image";
+import { dateFormater } from "@/utils/time";
+import { StaticPageParams } from "@/models/types";
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return articlePage;
+}
+
+export default function Page({ params }: StaticPageParams) {
+  const { article } = params;
+  const data = articles.find(({ id, ..._ }) => id === article);
+
+  return (
+    <div className={style["content-grid"]}>
+      <main className={`${style["main"]} ${style.breakout}`}>
+        {data ? (
+          <div>
+            <HeaderComp>
+              <Link href="/blog">Back</Link>
+              <h2>{data.title}</h2>
+            </HeaderComp>
+
+            <Row type='space'>
+              <Row>
+                <Image
+                  src={data.avatar}
+                  width={24}
+                  height={24}
+                  alt="esteban jimenez valdez avatar"
+                />
+                <p>{data.author}</p>
+              </Row>
+              <DateComp date={data.date.toString()}>
+                {dateFormater('en-US', data.date)}
+              </DateComp>
+            </Row>
+
+            <p>{data.description}</p>
+            {data.images.map(({
+              id,
+              image,
+              width,
+              height,
+              alt,
+            }) => (
+              <Image
+                key={id}
+                width={width}
+                height={height}
+                src={image}
+                alt={alt}
+              />
+            ))}
+          </div>
+        ) : (
+          <div>
+            <p>info dead</p>
+          </div>
+        )}
+      </main>
+    </div>
+  )
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import style from './page.module.css'
+import { Footer, Top } from '@/layouts'
+import Link from 'next/link'
+import { Card, FillButton, HeaderComp, IconTitle, Information } from '@/components'
+import { preview } from '../data/data'
+import Image from 'next/image'
+import { Arrow } from '../svg'
+
+const Blog = () => {
+  return (
+    <div className={style["content-grid"]}>
+      <div className={`${style["top"]} ${style.breakout}`}>
+        <Top />
+      </div>
+
+      <main className={`${style["main"]} ${style.breakout}`}>
+        <HeaderComp>
+          <Link href="/">Home</Link>
+          <h1>Blog page</h1>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque odit, adipisci voluptas doloribus, eveniet hic, provident facilis alias dignissimos iusto qui in error. Nostrum velit, at soluta quam itaque esse.</p>
+        </HeaderComp>
+
+        <Information gap='wide'>
+          {preview.map(({
+            id,
+            title,
+            icon,
+            description,
+            url,
+          }) => (
+            <Card outline key={id}>
+              <Information gap='normal'>
+                <IconTitle>
+                  <h4>{title}</h4>
+                  <Image
+                    src={icon}
+                    width={32}
+                    height={32}
+                    alt="cat smilling"
+                  />
+                </IconTitle>
+
+                <p>{description}</p>
+              </Information>
+
+              <Link href={url}>
+                <FillButton>
+                  check
+                  <Arrow color='light' disabled={false} />
+                </FillButton>
+              </Link>
+            </Card>
+          ))}
+        </Information>
+      </main>
+
+      <div className={`${style["bottom"]} ${style.breakout}`}>
+        <Footer />
+      </div>
+    </div>
+  )
+}
+
+export default Blog
