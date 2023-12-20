0a3a1fbddaca27ff2a8e01093b704e41905dd472
Esteban <wiwiwixd732@gmail.com>
2023-12-20 10:06:42 -0800

    feat: stack, old, future and tools data created

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,5 +1,5 @@
 /* Own */
-import { Links, Tutors, Pages, Videogames, Music } from "@/models/types";
+import { Links, Tutors, Pages, Videogames, Music, Technologies, Tools } from "@/models/types";
 import {
   alan,
   jack,
@@ -18,7 +18,6 @@ import { v4 as uuidv4 } from 'uuid';
 
 // Ver si puedo hacer un algoritmo junto a nodejs que me actualice mis datos que uso para la pagina, useando una funcion
 
-/* pages up-to-date */
 export const pages: Pages[] = [
   {
     id: uuidv4(),
@@ -158,7 +157,6 @@ export const tutors: Tutors[] = [
   },
 ];
 
-/* github links up-to-date */
 export const github: Links[] = [
   {
     id: uuidv4(),
@@ -346,5 +344,271 @@ export const music: Music[] = [
     id: uuidv4(),
     name: "Casiopea",
     albums: ["Galactic Funk", "Casiopea vs Tsquare - Fightman"],
+  },
+];
+
+export const stack: Technologies[] = [
+  {
+    id: uuidv4(),
+    name: "HTML",
+    image: catFace1,
+    alt: "html logo",
+  },
+  {
+    id: uuidv4(),
+    name: "CSS",
+    image: catFace1,
+    alt: "css logo",
+  },
+  {
+    id: uuidv4(),
+    name: "JavaScript",
+    image: catFace1,
+    alt: "javascript logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Typescript",
+    image: catFace1,
+    alt: "typescript logo",
+  },
+  {
+    id: uuidv4(),
+    name: "React",
+    image: catFace1,
+    alt: "react logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Nodejs",
+    image: catFace1,
+    alt: "nodejs logo",
+  },
+  {
+    id: uuidv4(),
+    name: "git",
+    image: catFace1,
+    alt: "git logo",
+  },
+  {
+    id: uuidv4(),
+    name: "pnpm",
+    image: catFace1,
+    alt: "pnpm logo",
+  },
+  {
+    id: uuidv4(),
+    name: "nextjs",
+    image: catFace1,
+    alt: "nextjs logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Astro",
+    image: catFace1,
+    alt: "astro logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Vite",
+    image: catFace1,
+    alt: "vite logo",
+  },
+  {
+    id: uuidv4(),
+    name: "CSS Modules",
+    image: catFace1,
+    alt: "css modules logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Testing Library",
+    image: catFace1,
+    alt: "testing library logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Jest",
+    image: catFace1,
+    alt: "jest logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Markdown",
+    image: catFace1,
+    alt: "markdown logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Github",
+    image: catFace1,
+    alt: "github logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Figma",
+    image: catFace1,
+    alt: "figma logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Notion",
+    image: catFace1,
+    alt: "notion logo",
+  },
+  {
+    id: uuidv4(),
+    name: "VS Code",
+    image: catFace1,
+    alt: "vscode logo",
+  },
+];
+
+export const old: Technologies[] =[
+  {
+    id: uuidv4(),
+    name: "Yarn",
+    image: catFace1,
+    alt: "yarn logo",
+  },
+  {
+    id: uuidv4(),
+    name: "npm",
+    image: catFace1,
+    alt: "npm logo",
+  },
+  {
+    id: uuidv4(),
+    name: "MUI",
+    image: catFace1,
+    alt: "mui logo",
+  },
+  {
+    id: uuidv4(),
+    name: "styled-components",
+    image: catFace1,
+    alt: "styled-components logo",
+  },
+  {
+    id: uuidv4(),
+    name: "tailwindcss",
+    image: catFace1,
+    alt: "tailwindcss logo",
+  },
+  {
+    id: uuidv4(),
+    name: "create-react-app",
+    image: catFace1,
+    alt: "create-react-app logo",
+  },
+  {
+    id: uuidv4(),
+    name: "mongodb",
+    image: catFace1,
+    alt: "mongodb logo",
+  },
+  {
+    id: uuidv4(),
+    name: "mongoose",
+    image: catFace1,
+    alt: "mongoose logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Supabase",
+    image: catFace1,
+    alt: "supabase logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Sanity",
+    image: catFace1,
+    alt: "sanity logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Firebase",
+    image: catFace1,
+    alt: "firebase logo",
+  },
+];
+
+export const future: Technologies[] =[
+  {
+    id: uuidv4(),
+    name: "rust",
+    image: catFace1,
+    alt: "rust logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Go",
+    image: catFace1,
+    alt: "go logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Prisma",
+    image: catFace1,
+    alt: "prisma logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Nest",
+    image: catFace1,
+    alt: "nest logo",
+  },
+  {
+    id: uuidv4(),
+    name: "fastify",
+    image: catFace1,
+    alt: "fastify logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Github Actions",
+    image: catFace1,
+    alt: "github actions logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Kubertenes",
+    image: catFace1,
+    alt: "kubertenes logo",
+  },
+  {
+    id: uuidv4(),
+    name: "Docker",
+    image: catFace1,
+    alt: "docker logo",
+  },
+  {
+    id: uuidv4(),
+    name: "AWS",
+    image: catFace1,
+    alt: "aws logo",
+  },
+  {
+    id: uuidv4(),
+    name: "lazyvim",
+    image: catFace1,
+    alt: "lazyvim logo",
+  },
+];
+
+export const tools: Tools[] = [
+  {
+    id: uuidv4(),
+    name: 'pkg-size',
+    image: catFace1,
+    alt: 'pkg-size logo',
+    url: 'https://pkg-size.dev/'
+  },
+  {
+    id: uuidv4(),
+    name: 'tsdocs.dev',
+    image: catFace1,
+    alt: 'tsdocs.dev logo',
+    url: 'https://tsdocs.dev/'
   },
 ];
