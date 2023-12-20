483d5c8373e61d18023622dd7c5b7cd931796989
Esteban <wiwiwixd732@gmail.com>
2023-12-20 10:28:45 -0800

    feat: all ready to be use

--- src/app/personal/page.tsx
+++ src/app/personal/page.tsx
@@ -15,56 +15,9 @@ const Projects = () => {
       <main className={`${style["main"]}`}>
         <HeaderComp>
           <Link href="/">Home</Link>
-          <h1>Bugs page</h1>
+          <h1>Personal page</h1>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque odit, adipisci voluptas doloribus, eveniet hic, provident facilis alias dignissimos iusto qui in error. Nostrum velit, at soluta quam itaque esse.</p>
         </HeaderComp>
-
-        <Information gap='wide'>
-          <h2>Games</h2>
-
-          <Row>
-            {videogames.map(({ id, game }) => (
-              <Key key={id}>
-                {game}
-              </Key>
-            ))}
-          </Row>
-        </Information>
-
-        <Information gap='wide'>
-          <h2>Sports</h2>
-
-          <Row>
-            {sports.map(({ id, game }) => (
-              <Key key={id}>
-                {game}
-              </Key>
-            ))}
-          </Row>
-        </Information>
-
-        <Information gap='wide'>
-          <h2>Music</h2>
-
-          <Information>
-            <Information gap='wide'>
-              {music.map(({ id, name, albums }) => (
-                <Information key={id}>
-                  <h4>
-                    {name}
-                  </h4>
-                  <Row>
-                    {albums.map((album) => (
-                      <Key key={id}>
-                        {album}
-                      </Key>
-                    ))}
-                  </Row>
-                </Information>
-              ))}
-            </Information>
-          </Information>
-        </Information>
 
         <Information>
           {github.map(({
