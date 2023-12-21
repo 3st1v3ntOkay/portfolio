9365f2186d61107725775fcf77255f6cfbd28fae
Esteban <wiwiwixd732@gmail.com>
2023-12-21 14:57:50 -0800

    feat: open graph set on metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,16 @@ export const metadata: Metadata = {
   title: "Me's Portfolio",
   description: "A programmer's Portfolio named Esteban Jimenez Valdez",
   // manifest: "/manifest.webmanifest",
+  openGraph: {
+    images: [
+      {
+        url: 'http://localhost:3000/api/og?title=Home',
+        width: 1200,
+        height: 630,
+        alt: 'home page'
+      },
+    ],
+  },
 }
 
 export default function RootLayout({
