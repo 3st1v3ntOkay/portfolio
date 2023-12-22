c94d114753ba267d30c991555638d7f967100934
Esteban <wiwiwixd732@gmail.com>
2023-12-22 13:52:14 -0800

    feat: bugs page removed and 2 new properties to pages object

--- src/app/data/routes.ts
+++ src/app/data/routes.ts
@@ -1,20 +1,16 @@
+import { catFace1, catFace2, catFace3, catFace4, catFace5 } from "@/assets/emoji";
 import { Pages } from "@/models/types";
 import { v4 as uuidv4 } from 'uuid';
 
 export const pages: Pages[] = [
   {
     id: uuidv4(),
-    page: "bugs page",
-    description: "Here you can find the programming languages and technologies I use for website development.",
-    url: '/bugs',
-    disabled: false,
-  },
-  {
-    id: uuidv4(),
     page: "blog page",
     description: "Here you can find the programming languages and technologies I use for website development.",
     url: '/blog',
     disabled: false,
+    icon: catFace1,
+    alt: 'happy cat emoji',
   },
   {
     id: uuidv4(),
@@ -22,6 +18,8 @@ export const pages: Pages[] = [
     description: "Here you can find the programming languages and technologies I use for website development.",
     url: '/follow',
     disabled: false,
+    icon: catFace2,
+    alt: 'cat emoji laughing',
   },
   {
     id: uuidv4(),
@@ -29,6 +27,8 @@ export const pages: Pages[] = [
     description: "Here you can find the programming languages and technologies I use for website development.",
     url: '/hobbies',
     disabled: false,
+    icon: catFace3,
+    alt: 'cat emoji laughing',
   },
   {
     id: uuidv4(),
@@ -36,6 +36,8 @@ export const pages: Pages[] = [
     description: "Here you can find the programming languages and technologies I use for website development.",
     url: '/personal',
     disabled: false,
+    icon: catFace4,
+    alt: 'cat emoji in love',
   },
   {
     id: uuidv4(),
@@ -43,5 +45,7 @@ export const pages: Pages[] = [
     description: "Here you can find the programming languages and technologies I use for website development.",
     url: '/work',
     disabled: false,
+    icon: catFace5,
+    alt: 'cat emoji mad',
   },
 ];
